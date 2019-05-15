a71ce61b06ea867f08fac47905c7ce74c4c5eea4
Olga Kuak <40384992+armanarr@users.noreply.github.com>
2019-05-15 20:00:25 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# python3 and pip install
+sudo apt-get install python3
+sudo ln -s /usr/bin/python3 /usr/bin/python
+python --version
+sudo apt-get update
+sudo apt-get install python3-pip
+
+#git install and project pull
+sudo apt-get install git
