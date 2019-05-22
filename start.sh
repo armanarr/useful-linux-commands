411cd7f1396666bf8e3d807a5e4865fd7d23873e
Olga Kuak <40384992+armanarr@users.noreply.github.com>
2019-05-22 14:04:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,35 @@ python --version
 sudo apt-get update
 sudo apt-get install python3-pip
 
-#git install and clone git repository
+#install python-libs from requirements.txt
+pip3 install -r requirements.txt
+
+#ssh-key create
+ssh-keygen -t rsa
+cat ~/.ssh/id_rsa.pub #for see it
+
+#for see all python process
+ps aux | grep python
+
+#kill process by pid
+kill -9 <PID>
+
+#for zip dir in file.zip
+zip -r file.zip config
+
+#download file
+wget <LINK>
+
+
+#--------------GIT---------------
+
 sudo apt-get install git
+git clone <LINK>
+git reset --hard #throws away all your uncommitted changes
+git pull origin master 
+
+#first push in git
+git init
+git commit -m "first commit"
+git remote add origin <REP LINK>
+git push -u origin master
